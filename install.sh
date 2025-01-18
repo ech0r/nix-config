08d43e10c30d07a7128ed29986c2616520419cf4
ech0r <john@coyote.tech>
2025-01-17 22:36:19 -0800

    latest

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export NIX_CONFIG="experimental-features nix-command flakes"
+
+nix run github:nix-community/disko/latest -- mode destroy,format,mount "./hosts/$1/disks.nix"
+
+nixos-generate-config --root /mnt
+
+nixos-install --flake .#nuc --impure
