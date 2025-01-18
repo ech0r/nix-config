9bae28a06e83824da5bb987f854f58b74ffb45c2
ech0r <john@coyote.tech>
2025-01-17 22:48:30 -0800

    latest

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 export NIX_CONFIG='experimental-features nix-command flakes'
 
-nix run github:nix-community/disko/latest -- mode destroy,format,mount "./hosts/$1/disks.nix"
+nix run github:nix-community/disko/latest -- --mode destroy,format,mount "./hosts/$1/disks.nix" --yes-wipe-all-disks
 
 nixos-generate-config --root /mnt
 
