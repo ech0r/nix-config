c4edfa91e3e96c98ab0a54d8692dfee4ca3b0090
ech0r <john@coyote.tech>
2025-01-28 20:34:02 -0800

    updated

--- hosts/nuc/nuc.nix
+++ hosts/nuc/nuc.nix
@@ -5,6 +5,7 @@
   # Import the hardware configuration dynamically
   imports = [
     ./hardware-configuration.nix
+    ../../shared/home-manager/home-manager.nix
   ];
   
   # Basic system settings
